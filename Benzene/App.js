7cb587be8181aedfc76761e05045215902ccb9ae
ayatamer <yuyu.tamer@gmail.com>
2020-08-13 17:28:06 +0200

    Update App.js

--- Benzene/App.js
+++ Benzene/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View,TouchableOpacity ,Image,Dimensions,ImageBackground} from 'react-native';
+import {TextInput, Platform, StyleSheet, Text, View,TouchableOpacity ,Image,Dimensions,ImageBackground} from 'react-native';
 import Tts from 'react-native-tts';
 import {Button, Icon} from 'native-base'
 import axios from 'axios'
@@ -13,24 +13,18 @@ import Voice, {
   SpeechErrorEvent,
 } from '@react-native-community/voice';
 import {NerManager} from 'node-nlp-rn'
-// import { w3cwebsocket as W3CWebSocket } from "websocket";
-// const client = new W3CWebSocket('ws://192.168.43.152:8000');
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     Voice.onSpeechError = this.onSpeechError;
     Voice.onSpeechResults = this.onSpeechResults;
-    this.socket = io("http://192.168.1.8:8000",{jsonp:false,transports:['websocket'],reconnection:false});
-    console.log(this.socket.connected)
-    this.socket.on('connect', () => {
-      console.log(this.socket.connected,"duuuude"); // true
-    });
-    this.socket.on('myres',res=>console.log('received res',res))
-    this.socket.on('connect_error',(err)=>console.log(err,err.code))
-    this.socket.on("chat", msg => {this.setState({ msg: msg});});
-    this.socket.on("rec",msg=>{
-      this.speak(msg['data'])})
-    this.socket.emit('req', "hello")
+    
+    this.state={
+      setSettings:false,
+      address:'',
+
+    }
   }
   
   componentDidMount(){
@@ -64,6 +58,21 @@ export default class App extends React.Component {
 
   
   return (
+   <React.Fragment>
+    {this.state.setSettings?<View>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => this.setState({address:text})}
+      value={this.state.address}
+    />
+    <Button transparent style={{width:'25%'}} onPress={()=>{
+      this.setState({setSettings:false},()=>this.animation.play())
+      
+    }}>
+      <Text style={{color:'#137FC9'}}>Back</Text>
+
+      </Button>
+      </View>:
     <View style={styles.container}>
     <Image resizeMode='stretch' style={{zIndex:1,width:Dimensions.get("window").width*.85,marginBottom:10,marginTop:30}} source={require('./assets/logo-t.png')} />
 
@@ -74,14 +83,35 @@ export default class App extends React.Component {
         }}
         source={require('./assets/robot-anim-1.json')}
       />
-      <Button block style={{borderRadius:10,backgroundColor:'#137FC9',position:'absolute',top:'77%',width:'90%',alignSelf:'center'}} onPress={()=>
+      <Button disabled={false} block style={{borderRadius:10,backgroundColor:'#137FC9',position:'absolute',top:'70%',width:'90%',alignSelf:'center'}} onPress={()=>
         // Voice.start('en-US')
         // this.socket.emit('req', "hello")
         Voice.start('en-US')
         }>
         <Text style={{color:'white'}}>Command</Text>
       </Button>
+      <Button block style={{borderRadius:10,backgroundColor:'#137FC9',position:'absolute',top:'80%',width:'90%',alignSelf:'center'}} onPress={()=>
+      {
+        this.socket = io(this.state.address,{jsonp:false,transports:['websocket'],reconnection:false});
+    console.log(this.socket.connected)
+    this.socket.on('connect', () => {
+      this.speak("server connected successfuly")
+      console.log(this.socket.connected,"duuuude"); // true
+    });
+    this.socket.on('connect_error',(err)=>console.log(err,err.code))
+    this.socket.on("rec",msg=>{
+      this.speak(msg['data'])})
+      }
+        }>
+        <Text style={{color:'white'}}>Connect</Text>
+      </Button>
+      <Button block transparent style={{width:'25%',top:'90%',position:'absolute'}} onPress={()=>this.setState({setSettings:true})}>
+      <Text style={{color:'#137FC9'}}>Settings</Text>
+
+      </Button>
     </View>
+    }
+    </React.Fragment>
   );
 }
 }
